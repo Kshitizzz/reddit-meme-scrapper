cf7ee2bb2c94265f17babe510127ffc3858f8e5f
Kshitiz <kshitizomar@gmail.com>
2021-04-17 20:45:16 +0530

    added module which writes to pdf

--- saveToPDF.js
+++ saveToPDF.js
@@ -5,14 +5,14 @@ let pdfDocument = require('pdfkit');
 async function saveToPDF(dirPath){
 
     let pdfDoc = new pdfDocument({size : 'A4'});
-    pdfDoc.font('Times-Roman').fontSize(40).fillColor('black').text('Piping Hot Memes', 
+    pdfDoc.font('Times-Roman').fontSize(50).fillColor('black').text('Piping Hot Memes', 
     {align : 'center', valign : 'center'});
     pdfDoc.pipe(fs.createWriteStream('./memes.pdf'));
     console.log("Source Created");
     let memes =  await fs.promises.readdir(dirPath);
     for(let meme of memes){
         console.log(meme);
-        await pdfDoc.addPage().image(path.join(dirPath, meme), {width:500, height:500}); 
+        await pdfDoc.addPage().image(path.join(dirPath, meme), {width:500, height:500, align: 'center', valign : 'center'}); 
     }
     pdfDoc.end();
 }

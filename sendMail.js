94770dc3e7ccaebfc84adacb6f27d7f1ab0986e8
Kshitiz <kshitizomar@gmail.com>
2021-04-17 23:55:40 +0530

    module used to send mail

---
+++ sendMail.js
@@ -0,0 +1,60 @@
+let nodemailer = require('nodemailer');
+let {email, password} = require("./crediantials.js");
+
+module.exports = {
+    sendMail : sendMail
+}
+
+
+async function sendMail(attachmentPath, receiver1, receiver2) {
+    let transporter = nodemailer.createTransport({
+    host: 'smtp.gmail.com',
+    port: 465,
+    secure: true,
+    auth: {
+        user: email, 
+        pass: password
+        }
+    });
+    
+    let bodyText =`
+    Heyo
+
+
+    Here's your daily dose of memes. Enjoy!
+    Sent by reddit-scrapper-bot made with love by Kshitiz!
+
+
+    Later!
+    `
+
+    let bodyHtml = `
+    <b>Heyo</b>
+
+
+    <br>Here's your daily dose of memes. Enjoy!
+    Sent by reddit-scrapper-bot made with love by Kshitiz!
+
+
+    Later! </br>
+    `
+
+    let toSend = await transporter.sendMail({
+      from: "kshitiz.omar.16ece@bml.edu.in", // sender address
+      to: receiver1, // list of receivers
+      cc: receiver2,
+      subject: "Piping Hot Memes Served Right In Your Inbox!", // Subject line
+      text: bodyText, 
+      html: bodyHtml, 
+      attachments : [
+          {
+              filename : "memes.pdf",
+              path : attachmentPath
+          }
+      ] 
+    }, (err, data) => {
+        if(err) console.log(err);
+        else console.log(data);
+        });
+};
+
